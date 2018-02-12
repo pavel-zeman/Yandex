5275e64d3832d3aaa962b1be9736f41b1c044315
Pavel Zeman <pavel@pavel.cz>
2018-02-12 11:33:06 +0100

    Added 2018 warm-up solutions

---
+++ 2018/warmup/InterviewTask.cpp
@@ -0,0 +1,131 @@
+// Description
+// https://contest.yandex.com/algorithm2018/contest/7456/problems/C/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(c, m) for(int c=0;c<(int)(m);c++)
+#define FORE(c, f, t) for(int c=(f);c<(t);c++)
+#define LLI long long int
+#define MOD 1000000007
+
+
+char ccc = 0;
+int getInt() {
+    int r = 0;
+    while (!(ccc >= '0' && ccc <= '9')) ccc = getc_unlocked(stdin);
+    while (ccc >= '0' && ccc <= '9') {
+        r = r * 10 + (ccc - '0');
+        ccc = getc_unlocked(stdin);
+    }
+    return r;
+}
+
+int getString(char *start) {
+    char *data = start;
+    while (!(ccc > ' ' && ccc <= '~')) ccc = getc_unlocked(stdin);
+    while (ccc > ' ' && ccc <= '~') {
+        *data++ = ccc;
+        ccc = getc_unlocked(stdin);
+    }
+    *data = 0;
+    return data - start;
+}
+
+
+int getSignedInt() {
+    int r = 0;
+    while (!(ccc == '-' || (ccc >= '0' && ccc <= '9'))) ccc = getc_unlocked(stdin);
+    bool minus = ccc == '-';
+    if (minus) ccc = getc_unlocked(stdin);
+    while (ccc >= '0' && ccc <= '9') {
+        r = r * 10 + (ccc - '0');
+        ccc = getc_unlocked(stdin);
+    }
+    return minus ? -r : r;
+}
+
+long long int getLongLongInt() {
+    long long int r = 0;
+    while (!(ccc >= '0' && ccc <= '9')) ccc = getc_unlocked(stdin);
+    while (ccc >= '0' && ccc <= '9') {
+        r = r * 10 + (ccc - '0');
+        ccc = getc_unlocked(stdin);
+    }
+    return r;
+}
+
+
+template <class type> void print(type a) {
+    if (a < 0) {
+        putc_unlocked('-', stdout);
+        a = -a;
+    }
+    if (a == 0) {
+        putc_unlocked('0', stdout);
+    } else {
+        char result[20];
+        int resSize = 0;
+        while (a > 0) {
+            result[resSize++] = '0' + a % 10;
+            a /= 10;
+        }
+        while (--resSize >= 0) putc_unlocked(result[resSize], stdout);
+    }
+    putc_unlocked('\n', stdout);
+}
+
+void printString(const char *str) {
+    while (*str) putc_unlocked(*str++, stdout);
+    putc_unlocked('\n', stdout);
+}
+
+int power(int a, int b) {
+    int sq = a;
+    int result = 1;
+    while (b > 0) {
+        if ((b & 1) == 1) result = result * (LLI)sq % MOD;
+        sq = sq * (LLI)sq % MOD;
+        b >>= 1;
+    }
+    return result;
+}
+
+int inv(int a) {
+    return power(a, MOD - 2);
+}
+
+template <class type> type gcd(type a, type b) {
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+void fail() {
+    *((char *)0) = 0;
+}
+
+double cpu() {
+    return clock() / (double)CLOCKS_PER_SEC;
+}
+
+int data[200000000];
+int data2[200000000];
+
+int main(void) {
+    int n = 2;
+    data[0] = data[1] = 1;
+    FOR(i, 27) {
+        int n2 = 0;
+        FOR(j, n - 1) {
+            data2[n2++] = data[j];
+            data2[n2++] = data[j] + data[j + 1];
+        }
+        data2[n2++] = data[n - 1];
+        memcpy(data, data2, sizeof(int) * n2);
+        n = n2;
+        int res = 0;
+        FOR(j, n) res += data[j] == (i + 2);
+        //FOR(j, n) printf("%d ", data[j]);
+        //printf("\n");
+        printf("%d %d %d\n", i + 2, n, res);
+    }
+}
+
---
+++ 2018/warmup/PalindromicFeature.cpp
@@ -0,0 +1,137 @@
+// Description
+// https://contest.yandex.com/algorithm2018/contest/7456/problems/B/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(c, m) for(int c=0;c<(int)(m);c++)
+#define FORE(c, f, t) for(int c=(f);c<(t);c++)
+#define LLI long long int
+#define MOD 1000000007
+
+
+char ccc = 0;
+int getInt() {
+    int r = 0;
+    while (!(ccc >= '0' && ccc <= '9')) ccc = getc_unlocked(stdin);
+    while (ccc >= '0' && ccc <= '9') {
+        r = r * 10 + (ccc - '0');
+        ccc = getc_unlocked(stdin);
+    }
+    return r;
+}
+
+int getString(char *start) {
+    char *data = start;
+    while (!(ccc > ' ' && ccc <= '~')) ccc = getc_unlocked(stdin);
+    while (ccc > ' ' && ccc <= '~') {
+        *data++ = ccc;
+        ccc = getc_unlocked(stdin);
+    }
+    *data = 0;
+    return data - start;
+}
+
+
+int getSignedInt() {
+    int r = 0;
+    while (!(ccc == '-' || (ccc >= '0' && ccc <= '9'))) ccc = getc_unlocked(stdin);
+    bool minus = ccc == '-';
+    if (minus) ccc = getc_unlocked(stdin);
+    while (ccc >= '0' && ccc <= '9') {
+        r = r * 10 + (ccc - '0');
+        ccc = getc_unlocked(stdin);
+    }
+    return minus ? -r : r;
+}
+
+long long int getLongLongInt() {
+    long long int r = 0;
+    while (!(ccc >= '0' && ccc <= '9')) ccc = getc_unlocked(stdin);
+    while (ccc >= '0' && ccc <= '9') {
+        r = r * 10 + (ccc - '0');
+        ccc = getc_unlocked(stdin);
+    }
+    return r;
+}
+
+
+template <class type> void print(type a) {
+    if (a < 0) {
+        putc_unlocked('-', stdout);
+        a = -a;
+    }
+    if (a == 0) {
+        putc_unlocked('0', stdout);
+    } else {
+        char result[20];
+        int resSize = 0;
+        while (a > 0) {
+            result[resSize++] = '0' + a % 10;
+            a /= 10;
+        }
+        while (--resSize >= 0) putc_unlocked(result[resSize], stdout);
+    }
+    putc_unlocked('\n', stdout);
+}
+
+void printString(const char *str) {
+    while (*str) putc_unlocked(*str++, stdout);
+    putc_unlocked('\n', stdout);
+}
+
+int power(int a, int b) {
+    int sq = a;
+    int result = 1;
+    while (b > 0) {
+        if ((b & 1) == 1) result = result * (LLI)sq % MOD;
+        sq = sq * (LLI)sq % MOD;
+        b >>= 1;
+    }
+    return result;
+}
+
+int inv(int a) {
+    return power(a, MOD - 2);
+}
+
+template <class type> type gcd(type a, type b) {
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+void fail() {
+    *((char *)0) = 0;
+}
+
+double cpu() {
+    return clock() / (double)CLOCKS_PER_SEC;
+}
+
+char data[200010];
+char res[4];
+
+int main(void) {
+    int l = getString(data);
+    res[0] = 'z';
+    res[1] = 'z';
+    res[2] = 0;
+    bool found = false;
+    FOR(i, l - 1) {
+        if (data[i] == data[i + 1] && (!found || data[i] < res[0])) {
+            found = true;
+            res[0] = res[1] = data[i];
+        }
+    }
+    if (!found) {
+        res[2] = 'z';
+        res[3] = 0;
+        FOR(i, l - 2) {
+            if (data[i] == data[i + 2] && (!found || strncmp(data + i, res, 3) < 0)) {
+                found = true;
+                res[0] = res[2] = data[i];
+                res[1] = data[i + 1];
+            }
+        }
+    }
+    if (found) printString(res); else print(-1);
+}
+
