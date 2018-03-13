51cb997b6142877821aa242873ad494177d89605
Pavel Zeman <pavel@pavel.cz>
2018-03-13 21:42:25 +0100

    Added 2018 qualification solutions

---
+++ 2018/qualification/Lottery.cpp
@@ -0,0 +1,120 @@
+// Description
+// https://contest.yandex.com/algorithm2018/contest/7491/problems/A/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(c, m) for(int c=0;c<(int)(m);c++)
+#define FORE(c, f, t) for(int c=(f);c<(t);c++)
+#define LLI long long int
+#define MOD 1000000007
+
+
+char ccc = 0;
+int getInt() {
+    int r = 0;
+    while (!(ccc >= '0' && ccc <= '9')) ccc = getc_unlocked(stdin);
+    while (ccc >= '0' && ccc <= '9') {
+        r = r * 10 + (ccc - '0');
+        ccc = getc_unlocked(stdin);
+    }
+    return r;
+}
+
+int getString(char *start) {
+    char *data = start;
+    while (!(ccc > ' ' && ccc <= '~')) ccc = getc_unlocked(stdin);
+    while (ccc > ' ' && ccc <= '~') {
+        *data++ = ccc;
+        ccc = getc_unlocked(stdin);
+    }
+    *data = 0;
+    return data - start;
+}
+
+
+int getSignedInt() {
+    int r = 0;
+    while (!(ccc == '-' || (ccc >= '0' && ccc <= '9'))) ccc = getc_unlocked(stdin);
+    bool minus = ccc == '-';
+    if (minus) ccc = getc_unlocked(stdin);
+    while (ccc >= '0' && ccc <= '9') {
+        r = r * 10 + (ccc - '0');
+        ccc = getc_unlocked(stdin);
+    }
+    return minus ? -r : r;
+}
+
+long long int getLongLongInt() {
+    long long int r = 0;
+    while (!(ccc >= '0' && ccc <= '9')) ccc = getc_unlocked(stdin);
+    while (ccc >= '0' && ccc <= '9') {
+        r = r * 10 + (ccc - '0');
+        ccc = getc_unlocked(stdin);
+    }
+    return r;
+}
+
+
+template <class type> void print(type a) {
+    if (a < 0) {
+        putc_unlocked('-', stdout);
+        a = -a;
+    }
+    if (a == 0) {
+        putc_unlocked('0', stdout);
+    } else {
+        char result[20];
+        int resSize = 0;
+        while (a > 0) {
+            result[resSize++] = '0' + a % 10;
+            a /= 10;
+        }
+        while (--resSize >= 0) putc_unlocked(result[resSize], stdout);
+    }
+    putc_unlocked('\n', stdout);
+}
+
+void printString(const char *str) {
+    while (*str) putc_unlocked(*str++, stdout);
+    putc_unlocked('\n', stdout);
+}
+
+int power(int a, int b) {
+    int sq = a;
+    int result = 1;
+    while (b > 0) {
+        if ((b & 1) == 1) result = result * (LLI)sq % MOD;
+        sq = sq * (LLI)sq % MOD;
+        b >>= 1;
+    }
+    return result;
+}
+
+int inv(int a) {
+    return power(a, MOD - 2);
+}
+
+template <class type> type gcd(type a, type b) {
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+void fail() {
+    *((char *)0) = 0;
+}
+
+double cpu() {
+    return clock() / (double)CLOCKS_PER_SEC;
+}
+
+bool win[100];
+
+int main(void) {
+    FOR(i, 10) win[getInt()] = true;
+    int n = getInt();
+    FOR(i, n) {
+        int total = 0;
+        FOR(j, 6) total += win[getInt()];
+        printString(total >= 3 ? "Lucky" : "Unlucky");
+    }
+}
+
